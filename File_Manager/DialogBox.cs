0608dbd99887dc47619f2dfdfee7c69b366cc1ec
MathMark <vadimmartsun@gmail.com>
2015-11-22 18:28:48 +0300

    class "Navigator" was moved to another file and has rewritten. (reverted from commit 2e437b9136afbf830c4e64e71981cb982a574019)

--- File_Manager/DialogBox.cs
+++ File_Manager/DialogBox.cs
@@ -17,74 +17,74 @@ namespace File_Manager
             InitializeComponent();
         }
 
-//        public static string name;
-//        public static string format;
+        public static string name;
+        public static string format;
 
-//        public static string way;
+        public static string way;
 
-//        public static bool done = false;
+        public static bool done = false;
 
 
 
-//        private void button2_Click(object sender, EventArgs e)
-//        {
-//            name = Name.Text;
-//            try
-//            {
-//                switch(Form1.createThing)
-//                {
-//                    case 1:
-//                        Directory.CreateDirectory(way + name);
-//                        break;
-//                    case 2:
-//                        File.Create(way + name);
-//                        break;
-//                    case 3:
-//                         File.Create(way + name+".xlsx");
-//                        break;
-//                    case 4:
-//                         File.Create(way+ name+".docx");
-//                        break;
-//                    case 5:
-//                        File.Create(way + name+".txt");
-//                        break;
-//                    case 6:
-//                        if (name != string.Empty)
-//                        {
-//                            if (format != string.Empty)
-//                            {
-//                                File.Move(Form1.LeftPath + Form1.lastname, Form1.LeftPath + name + format);
-//                            }
-//                            else
-//                            {
-//                                Directory.Move(way + Form1.lastname, way + name);
-//                            }
-//                            done = true;
-//                        }
-//                        else
-//                        {
-//                            MessageBox.Show("You haven't entered the name", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-//                        }
-//                        break;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            name = Name.Text;
+            try
+            {
+                switch(Form1.createThing)
+                {
+                    case 1:
+                        Directory.CreateDirectory(way + name);
+                        break;
+                    case 2:
+                        File.Create(way + name);
+                        break;
+                    case 3:
+                         File.Create(way + name+".xlsx");
+                        break;
+                    case 4:
+                         File.Create(way+ name+".docx");
+                        break;
+                    case 5:
+                        File.Create(way + name+".txt");
+                        break;
+                    case 6:
+                        if (name != string.Empty)
+                        {
+                            if (format != string.Empty)
+                            {
+                                File.Move(Form1.LeftPath + Form1.lastname, Form1.LeftPath + name + format);
+                            }
+                            else
+                            {
+                                Directory.Move(way + Form1.lastname, way + name);
+                            }
+                            done = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("You haven't entered the name", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                        }
+                        break;
 
-//            }
-//            }
-//            catch (UnauthorizedAccessException)
-//            {
-//                MessageBox.Show("You haven't named this file", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-//                goto q;
-//            }
-//            catch (IOException)
-//            {
-//                MessageBox.Show("Error in syntax", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-//                goto q;
-//            }
-//            catch(ArgumentException)
-//                {
-//                    MessageBox.Show("Error in syntax", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
-//                    goto q;
-//                }
-//            q:this.Close();
-//        }
-   }
+            }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("You haven't named this file", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                goto q;
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error in syntax", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                goto q;
+            }
+            catch(ArgumentException)
+                {
+                    MessageBox.Show("Error in syntax", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                    goto q;
+                }
+            q:this.Close();
+        }
+    }
 }
