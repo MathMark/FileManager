74a8568b04c54324f51488d183c130f006079217
MathMark <vadimmartsun@gmail.com>
2015-11-27 17:56:36 +0300

    Added new class that will help to start threads for methods "Copy" and "Move"

---
+++ File_Manager/Tonnel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace File_Manager
+{
+   public class Tonnel
+    {
+        public string SourcePath { get; private set; }
+        public string DestinationPath { get; private set; }
+        public string SelectedFile { get; private set; }
+        public int value = 0;
+
+        public Tonnel(string SelectedFile,string SourcePath,string DestinationPath)
+        {
+            this.SelectedFile = SelectedFile;
+            this.SourcePath = SourcePath;
+            this.DestinationPath = DestinationPath;
+        }
+    }
+}
